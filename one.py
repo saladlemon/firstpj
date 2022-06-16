9207ab69feb293eefd18b65924f05fa0569d43a1
saladlemon <107619587+saladlemon@users.noreply.github.com>
2022-06-16 22:55:20 +0900

    update

--- one.py
+++ one.py
@@ -1,2 +1,19 @@
 print("Hello dohoon world")
 print("changed this project")
+
+print("강한친구 대한육군\n강한친구 대한육군")
+
+print(r"\    /\ ")
+print(r" )  ( ') ")
+print(r"(  /  )")
+print(r" \(__)|")
+
+print(r"|\_/|")
+print(r"|q p|   /}")
+print("( 0 )\"\"\"\ ")
+print("|\"^\"`    |")
+print(r"||_/=\\__| ")
+
+a = input()
+b = input()
+print(a+b)
